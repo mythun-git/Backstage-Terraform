248a64bd255e12f4fc12aaab9efa23fc540915aa
MITHUN <82029184+mythun-git@users.noreply.github.com>
2024-02-05 14:48:49 +0530

    Update main.tf

--- content/main.tf
+++ content/main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = ${{ values.bucketRegion }} 
 }
 
-resource "aws_s3_bucket" "microsegmentations3bucket" #${{ parameters.name }}
-  bucket = ${{ values.bucketName }} # { #${{ parameters.bucketName }}
+resource "aws_s3_bucket" "${{values.name}}"
+  bucket = "${{ values.bucketName }}"
   
 }
 
 resource "aws_s3_access_point" "msaccesspoint" {
   name          = "access-point"
-  bucket        = aws_s3_bucket.microsegmentations3bucket.bucket  ## aws_s3_bucket.${{ parameters.name }}.bucket
+  bucket        = aws_s3_bucket.${{values.name}}.bucket 
   vpc_configuration {
     vpc_id = "vpc-005aa4d379e350404"
   }
