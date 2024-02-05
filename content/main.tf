1af15229c0033905967df204ad9c4d70271c65ea
MITHUN <82029184+mythun-git@users.noreply.github.com>
2024-02-05 14:45:48 +0530

    Update main.tf

--- content/main.tf
+++ content/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = ${{ parameters.bucketRegion }} 
+  region = ${{ values.bucketRegion }} 
 }
 
 resource "aws_s3_bucket" "microsegmentations3bucket" #${{ parameters.name }}
